924fb2163dfb3f75055a16c9c6e4ce550bbbc2ab
vouching <v_ned@mail.ru>
2018-10-06 23:09:03 +0300

    test3059

---
+++ 3059.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a;
+    b=1;
+    while(b<=a);
+    {
+        cout<<b<<endl;
+        b*=2;
+    }
+
+}
