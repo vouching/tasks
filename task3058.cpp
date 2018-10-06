5a26fa34ecf58089a81938113f0b9f7f62841d1c
vouching <v_ned@mail.ru>
2018-10-06 22:30:49 +0300

    start of task 3058

---
+++ task3058.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int a,i;
+    while(i<=a)
+    {
+        if(a%i==0)
+        {
+            cout<<i;
+        }
+        i++;
+    }
+}
