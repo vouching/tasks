8a057ac05ee3f81302a0d574a8095ce3909ebc6d
vouching <v_ned@mail.ru>
2018-10-06 22:23:12 +0300

    test

--- task113.cpp
+++ task113.cpp
@@ -5,4 +5,14 @@
 using namespace std;
 int main()
 {
+    int a,g;
+    double i;
+    cin>>a;
+    i=1;
+    while(i<=a)
+    {
+        g= (int) sqrt(i);
+        if(g==sqrt(i)) cout<<g*g<<endl;
+        i++;
+    }
 }
